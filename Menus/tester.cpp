1c7302120f0f97b895c14c0cd504f7dab9910a49
Axolotlpi <47962588+Axolotlpi@users.noreply.github.com>
2020-11-22 15:27:05 -0600

    Deleted previous tested things

--- Menus/tester.cpp
+++ Menus/tester.cpp
@@ -7,14 +7,6 @@ using namespace std;
 
 int main(){
 
-    // int whole = 3;
-    // double dec = 5.5;
-    // string word = "hello";
-    // char letter = 'i';
-
-    // int *num = &whole;
-
-    // cout << *num << endl;
     
     vector<int> myvector; 
     myvector.push_back(6);
